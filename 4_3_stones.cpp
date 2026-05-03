fbba59109c791977e13df72225ced39550a788cb
sharma-sheetal <sharma.sheetal.oct2003@gmail.com>
2026-05-03 17:43:51 +0530

    DONE

--- 4_3_stones.cpp
+++ 4_3_stones.cpp
@@ -10,46 +10,51 @@ Within a given sequence of stones, find minimum cost to remove all of them.
  see comments (approach by vgtcross)
 */
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
+ // Question Link :- https://codeforces.com/problemset/problem/358/D
+
+#include<bits/stdc++.h>
 using namespace std;
 
-
-long long min_removal_cost(int n, const vector<long long>& a, const vector<long long>& b) {
-    // dp[i][0] = min cost for stones 1..i if stone i+1 is removed before stone i
-    // dp[i][1] = min cost for stones 1..i if stone i+1 is removed after stone i
-    vector<vector<long long>> dp(n + 1, vector<long long>(2, 0));
-   
-    // Base case for the first stone
-    dp[1][0] = 0;
-    dp[1][1] = a[0];
-   
-    // Fill dp table for stones 2..n
-    for (int i = 2; i <= n; ++i) {
-        dp[i][0] = min(dp[i - 1][0] + a[i - 1], dp[i - 1][1] + 0);
-        dp[i][1] = min(dp[i - 1][0] + b[i - 1], dp[i - 1][1] + a[i - 1]);
+int solve(vector<int> &a, vector<int> &b, vector<int> &c, int n){
+    
+    if(n<=1)
+    return a[0];
+    
+    vector<vector<int>> dp(n, vector<int>(2, 0));
+    dp[0][0] = a[0];
+    dp[0][1] = b[0];
+    
+    for(int i=1; i<n; i++){
+        dp[i][0] = max(dp[i-1][0]+b[i], dp[i-1][1]+a[i]);
+        dp[i][1] = max(dp[i-1][1]+b[i], dp[i-1][0] + c[i]);
     }
-   
-    // The answer is dp[n][0] because stone n+1 doesn't exist
-    return dp[n][0];
+    
+    return dp[n-1][0];
 }
 
 
-int main() {
+int main(){
+    
     int n;
-    cin >> n;
-   
-    vector<long long> a(n), b(n);
-    // Read cost array a (cost if stone is removed before its neighbor)
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+    cin>>n;
+    
+    vector<int> a(n), b(n), c(n);
+    for(int i=0; i<n; i++){
+        cin>>a[i];
     }
-    // Read cost array b (cost if stone is removed after its neighbor)
-    for (int i = 0; i < n; ++i) {
-        cin >> b[i];
+    for(int i=0; i<n; i++){
+        cin>>b[i];
     }
-   
-    cout << min_removal_cost(n, a, b) << endl;
+    for(int i=0; i<n; i++){
+        cin>>c[i];
+    }
+    
+    int ans= solve(a, b, c, n);
+    cout<<ans;
+    
+    
+    
     return 0;
 }
+
+************************************************************* DONE *******************************************************
